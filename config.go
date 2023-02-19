387da0602db775c3843471459483bc462b15e1cd
Dylan Cant <dylanjessecant@gmail.com>
2023-02-19 00:09:15 -0500

    updated version to 0.2

--- config.go
+++ config.go
@@ -7,7 +7,7 @@ import (
 )
 
 const relay_url = "ws://localhost"
-const nip11_info_document = "{\"name\":\"GNOST Relay\",\"description\":\"GNOST Relay\",\"pubkey\":\"0000000000000000000000000000000000000000000000000000000000000000\",\"contact\":\"barkyq\",\"supported_nips\":[9,12,15,16,20,22,26,28,33,42],\"software\":\"git+https://github.com/barkyq/gnost-relay\",\"version\":\"0.1\"}"
+const nip11_info_document = "{\"name\":\"GNOST Relay\",\"description\":\"GNOST Relay\",\"pubkey\":\"0000000000000000000000000000000000000000000000000000000000000000\",\"contact\":\"barkyq\",\"supported_nips\":[9,12,15,16,20,22,26,28,33,42],\"software\":\"git+https://github.com/barkyq/gnost-relay\",\"version\":\"0.2\"}"
 
 const max_limit = 25
 const subid_max_length = 32
